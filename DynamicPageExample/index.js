7cab7b7c4c14fb83a9387542159ca722fffb9f79
Preetham <j.p.rao1327@gmail.com>
2024-03-02 22:58:44 +0530

    adding the dynamic page example

---
+++ DynamicPageExample/index.js
@@ -0,0 +1,14 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+};
+let pet = null
+let petImageEl = document.getElementById("petImg")
+let petSelectEl = document.getElementById("petSelect");
+petSelectEl.addEventListener("change", function(event) {
+    pet = event.target.value;
+    petImageEl.src = petsImageUrls[pet];
+})
